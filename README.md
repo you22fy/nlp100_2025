24096c42d19f0bd8d396326924b082baa57323fc
you22fy <uni22yu@gmail.com>
2025-04-08 23:51:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nlp100本ノック(2025)
+[これ](https://nlp100.github.io/2025/ja/index.html)をやる‼️
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from nlp100-2025!")
+
+
+if __name__ == "__main__":
+    main()
